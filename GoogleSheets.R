df41e365f846d0f5d6b27d19c46d5ed0b8b53c47
Anthony <anthonyebert@gmail.com>
2018-11-13 13:09:08 +1000

    Trying to get publication list from Google Scholar.

--- GoogleSheets.R
+++ GoogleSheets.R
@@ -4,10 +4,17 @@
 library(googlesheets4)
 library(dplyr)
 library(stringr)
+loadNamespace("scholar")
 
 ACEMS_people <-
   googlesheets4::read_sheet(ss = "https://docs.google.com/spreadsheets/d/1g1HP35bvlaOdDmx5zvDkI2e1diHOePkzgQPaEqmGnLk/edit?usp=sharing")
 
 GoogleIDs <-
-  ACEMS_people %>% select(`Google Scholar ID`) %>% filter(stringr::str_detect(`Google Scholar ID`, "AAAAJ"))
-                                                          
+  ACEMS_people %>% select(`Google Scholar ID`) %>% filter(stringr::str_detect(`Google Scholar ID`, "AAAAJ")) %>%
+  mutate(scholarstr = `Google Scholar ID`) %>%
+  group_by(scholarstr) %>%
+  mutate(scholarID = substr(scholarstr, str_locate(pattern = "AAAAJ", scholarstr)[1] - 7, str_locate(pattern = "AAAAJ", scholarstr)[2])) %>%
+  ungroup() %>%
+  select(scholarID)
+
+Publication_list <- GoogleIDs[1:5,] %>% group_by(scholarID) %>% do(scholar::get_publications(.$scholarID))
